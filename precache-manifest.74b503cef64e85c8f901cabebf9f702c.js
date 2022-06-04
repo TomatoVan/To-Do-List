d377c4e1f817c9d237661beeed3b57c2588230d3
Leo <kovzel.leonid@mail.ru>
2022-06-04 20:11:36 +0300

    Updates

---
+++ precache-manifest.74b503cef64e85c8f901cabebf9f702c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac222eacd774eea1c14e9bcc0a38db3",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "f07e2207af98657b4c3b",
+    "url": "/Todolist/static/css/main.4f988151.chunk.css"
+  },
+  {
+    "revision": "1cd796ac92bd3963d950",
+    "url": "/Todolist/static/js/2.476c371c.chunk.js"
+  },
+  {
+    "revision": "4d522234985867aefb3dcdf12a73d507",
+    "url": "/Todolist/static/js/2.476c371c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07e2207af98657b4c3b",
+    "url": "/Todolist/static/js/main.6fd51241.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
